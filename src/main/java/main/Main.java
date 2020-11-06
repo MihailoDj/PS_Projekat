065a90427a7648139be824f6a82c1490f526419e
Mihailo Djordjevic <mihailodjordjevic392@gmail.com>
2020-11-07 00:58:52 +0100

    Netbeans remote push bug - netacni kredencijali

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -14,5 +14,6 @@ import forme.FrmPrijavljivanje;
 public class Main {
     public static void main(String[] args) {
         new FrmPrijavljivanje().setVisible(true);
+        //Netbeans remote push test
     }
 }
